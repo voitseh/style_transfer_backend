126beb1f39acbbbeefa9b8c599eb69888718cec1
voitseh <voitseh.kaminsky@gmail.com>
2018-04-18 17:24:03 +0300

    Refactored main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, g, url_for
+from os.path import exists
 from flask_cors import CORS
 from flask import render_template
 from gevent.wsgi import WSGIServer
@@ -7,10 +8,10 @@ import flask_sijax
 import os, glob
 import  base64 
 import numpy as np
-import logging
 import PIL.Image
 import stylize_image
 import utils
+
 
 # The path where you want the extension to create the needed javascript files
 # DON'T put any of your files in this directory, because they'll be deleted!
@@ -28,156 +29,205 @@ STYLES_FOLDER = os.path.join(UPLOAD_FOLDER, 'style_gallery')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024
 
-gallery_ID = ""
 
-#################### PROCESS IMAGE ###############################
-def decode_img(b64file, f_name):
-    if ',' in b64file:
-        imgdata = b64file.split(',')[1]
-        decoded = base64.b64decode(imgdata)
+class StylesGallery:
 
-        utils.write_file(f_name, decoded)
+    __gallery_id = 'style_gallery'
+
+    def __init__(self, obj_response):
+        global STYLES_FOLDER
+        self.__obj_response = obj_response
+  
+    def add_new_style(self,filename, base64_img):
+        style_path =  os.path.join(STYLES_FOLDER, filename)
+        utils.decode_img(base64_img, style_path)
+        utils.make_thumbnail(style_path)
     
-      
-
-def make_thumbnail(filename):
-    thumbnail_size = (300,300)
-    im = Image.open(filename)
-    im.thumbnail(thumbnail_size,  Image.ANTIALIAS)
-    im.save(filename)
-
-def encode_img(filename):
-    with open(filename, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    return encoded_string
-
-def process_base64(filename):
-    filename = str(filename).split("b'")[-1].split("b'")[-1][:-1]
-    filename = 'data:image/png;base64,{}{}'.format(filename,'=') if filename.endswith('=') else  'data:image/png;base64,{}'.format(filename)
-   
-    return filename
-
-#################### FOR STYLIZED IMAGE ###############################
-
-def remove_img(obj_response, img_id):
-    obj_response.script("$('#%s').remove()"%(img_id));
-
-def show_img(obj_response, parent_id, img_id, new_img, img_name):
-    # process base64 image to be readeble in browser
-    new_img = process_base64(new_img)
-    obj_response.script("$('#%s').append($('<img>',{ style:'position:absolute; left:0px; top:0px; z-index: 2',id:'%s',src:'%s', name:'%s'}));"%(parent_id, img_id, new_img, img_name))
- 
-def change_img(obj_response, parent_id, img_id, new_img, img_name):
-    #remove previous image from gallery
-    if img_id != None:
-        remove_img(obj_response, img_id);
-    #send new image to frame
-    show_img(obj_response, parent_id, img_id, new_img, img_name)
-
-def response(obj_response, parent_id, img_id, filename):
-    new_img = encode_img(filename)
-    img_name = filename.split('/')[-1]
-    change_img(obj_response, parent_id, img_id, new_img, img_name)
+    def delete_style(self, filename):
+        style_path = os.path.join(STYLES_FOLDER,filename)
+        assert exists(style_path), "Style you want delete not found!"
+        os.remove(style_path)
     
-###################### STYLES GALLERY ##############################
+    def check_styles_folder_exists(self):
+        assert exists(STYLES_FOLDER), "STYLES_FOLDER not found!"
 
-def remove_gallery_imgs(obj_response, parent_id):
-    obj_response.script("$('#%s').children().remove()"%(parent_id));
-
-def show_gallery_img(obj_response, parent_id, img_id, new_img, del_id):
-    img_name = new_img.split('style_gallery/')[-1]  
-    file_address = UPLOAD_FOLDER + new_img.split('images')[-1]
-    new_img = encode_img(file_address)
-    new_img = process_base64(new_img)
-    obj_response.script("$('#%s').append($('<li>',{class:'image_grid'}).append($('<a href=#>').append($('<label>').append($('<img>',{id:'%s', src:'%s', name:'%s'})).append($('<input>',{type:'radio', name:'selimg'})).append($('<span>',{class:'caption'})).append($('<span>',{id:'%s'})))));"%(parent_id, img_id, new_img, img_name, del_id))
-   
-def response_to_gallery(obj_response, parent_id, img_id, filename, del_id):
-    filename = filename.split('/')[-1]
-    result = url_for('static', filename='images/{}/{}'.format(parent_id,filename))
-    show_gallery_img(obj_response, parent_id, img_id, result, del_id) 
-
-def fill_gallery(obj_response, src_dir, gallery_ID):
-    index = 0
-    remove_gallery_imgs(obj_response, gallery_ID)
-    if os.path.exists(src_dir):
-        for filename in glob.glob(os.path.join( src_dir, "*.*")):
-            img_id = '_img{}'.format(str(index))
-            del_id = 'del_style{}'.format(str(index)) 
-            index += 1
-            #senf new image to frame
-            response_to_gallery(obj_response, gallery_ID, img_id, filename, del_id) 
-    else: 
-         print("{} is not exist!".format(src_dir))
-
-class SijaxHandler(object):
+    def reload_gallery_imgs(self):
+        self.__remove_gallery_imgs()
+        self.__load_gallery_imgs()
     
-    """A container class for all Sijax handlers.
-    Grouping all Sijax handler functions in a class
-    (or a Python module) allows them all to be registered with
-    a single line of code.
-    """
-    @staticmethod
-    def client_data(obj_response, client_data):
-      
-        selected_style = ''
-       
-        if  'message' in client_data:
-            obj_response.html("#server_response", "Stylize..")
+    def __remove_gallery_imgs(self):
+        self.__obj_response.script("$('#%s').children().remove()"%(self.__gallery_id));
     
-        if  'data' in client_data:
-            filename =  os.path.join(UPLOAD_FOLDER, 'photo_image.jpg')
+    def __load_gallery_imgs(self):
+        self.__id_index = 0 
+        for filename in glob.glob(os.path.join( STYLES_FOLDER, "*.*")):
+            self.__response_style_img_to_gallery(filename)
+  
+    def __response_style_img_to_gallery(self, img_path):
+        img_name, img_base64, img_id, img_delete_bttn_id = self.__get_style_img_attributes(img_path)
+        self.__obj_response.script("$('#%s').append($('<li>',{class:'image_grid'}).append($('<a href=#>').append($('<label>').append($('<img>',{id:'%s', src:'%s', name:'%s'})).append($('<input>',{type:'radio', name:'selimg'})).append($('<span>',{class:'caption'})).append($('<span>',{id:'%s'})))));"%(self.__gallery_id, img_id, img_base64, img_name, img_delete_bttn_id))
+     
+    def __get_style_img_attributes(self, img_path):
+        img_name = img_path.split('/')[-1]
+        img_base64 = self.__make_base64_encoded_string(img_path)
+        img_id = '_img{}'.format(str(self.__id_index))
+        img_delete_bttn_id = 'del_style{}'.format(str(self.__id_index)) 
+        self.__id_index += 1
+        return img_name, img_base64, img_id, img_delete_bttn_id
+    
+    def __make_base64_encoded_string(self, img_path):
+        encoded_img = utils.encode_img(img_path)
+        return utils.process_base64(encoded_img)
 
-            decode_img(client_data['data'][1], filename)
-            
-            #make 3 channels for png image
-            image = PIL.Image.open(filename)
-            im = image.convert("RGB")
-            im.save(filename)
-            
-            selected_style = client_data['data'][2].split('.')[0]
-            if client_data['data'][1] != '' and client_data['data'][3] != '':
-                content = os.path.join(UPLOAD_FOLDER, filename )
-                network = os.path.join(app.root_path, "pretrained-networks/{}".format(selected_style))
-                output = utils.rename_file(UPLOAD_FOLDER,'styled-', 'jpg')
-                
-                try:
-                    os.system("python {} {} {} {} {} {} {}".format(os.path.join(app.root_path, "stylize_image.py "),"--content ",content," --network-path ", network," --output-path ",output))
-                    response(obj_response, 'card_result', 'stylized_img', output)
-                    utils.remove_file(client_data['data'][0], UPLOAD_FOLDER)
-                    utils.remove_file(output, UPLOAD_FOLDER)
-                    obj_response.html('#server_response',  "")
-                except Exception as e:
-                    logging.exception("%s" % e)
-                    #obj_response.html('#server_response',  "%s" % e)
-                    obj_response.html('#server_response',  "Mistake!")
+    
 
-        # handle deleted styles gallery image
-        if  'delGalleryImg' in client_data:
-            filename = client_data['delGalleryImg']
-            utils.remove_file(filename, STYLES_FOLDER) 
+
+class ContentImageProcessor:
+    
+    def __init__(self, encoded_content_img):
+        global UPLOAD_FOLDER
+        self.__content_img_path = os.path.join(UPLOAD_FOLDER, 'photo_image.jpg')
+        self.__encoded_content_img = encoded_content_img
+    
+    def get_content_img_path(self):
+        return self.__content_img_path
+    
+    def process_content_img(self):
+        self.__decode_content_img()
+        self.__convert_content_img_to_3_channels()
+
+    def __decode_content_img(self):
+        return utils.decode_img(self.__encoded_content_img, self.__content_img_path)
         
-        # Fill styles gallery
-        if  'style_gallery' in client_data:
-            fill_gallery(obj_response, STYLES_FOLDER, client_data['style_gallery'])
-           
-        ############## styles gallery ###############################
-        # handle loading new style file to send it into styles gallery
-        if 'styleFile' in client_data:
-            f_name =  os.path.join(STYLES_FOLDER, client_data['styleFile'][0])
-            decode_img(client_data['styleFile'][1], f_name)
-            make_thumbnail(f_name)
-            fill_gallery(obj_response, STYLES_FOLDER, 'style_gallery')
+    def __convert_content_img_to_3_channels(self):
+        return utils.png_to_3channels(self.__content_img_path)
+        
+
+class ServerResponder():
+
+    def __init__(self, obj_response):
+        self.__obj_response = obj_response
+    
+    def send_message(self, message_type):
+        self.__obj_response.html("#server_response", message_type)
+
+    def send_image(self, img_base64):
+        img_name, img_base64 = self.__get_img_attributes(img_base64)
+        self.__obj_response.script("$('#%s').append($('<img>',{ style:'position:absolute; left:0px; top:0px; z-index: 2',id:'%s',name:'%s',src:'%s'}));"%('card_result', 'stylized_img', img_name, img_base64))
+    
+    def __get_img_attributes(self, img_base64):
+        img_name = utils.get_filename()
+        img_base64 = utils.add_charakters_to_base64_str(img_base64, False)
+        return img_name, img_base64
+    
+class ImageStylizer:
+    
+    def __init__(self, content_path, network_path):
+        self.__content = content_path
+        self.__network = network_path
+        
+    def stylize_image(self):
+        return stylize_image.main(self.__content, self.__network)
+       
 
 @flask_sijax.route(app, "/")
 def index():
     if g.sijax.is_sijax_request:
         # Sijax request detected - let Sijax handle it
-        g.sijax.register_callback('client_data', SijaxHandler.client_data) 
+        g.sijax.register_callback('client_data', Dispatcher().dispatch) 
         # The request looks like a valid Sijax request
         # The handlers are already registered above.. we can process the request
         return g.sijax.process_request()
     return render_template(index.html)
+
+"""A container class for all Sijax handlers.
+    Grouping all Sijax handler functions in a class
+    (or a Python module) allows them all to be registered with
+    a single line of code.
+"""
+class Dispatcher(object):
+
+    __CONTENT = ''
+    __NETWORK = ''
+
+    __obj_response = ''
+    __client_data = ''
+    
+    __initial_message = 'Stylize..'
+    __cleared_massage = ''
+
+    __styles_gallery = None
+    __server_responder = None
+   
+    def dispatch(self, obj_response, client_data):
+
+        self.__obj_response = obj_response
+        self.__client_data = client_data
         
+        self.__make_instances()
+        
+        self.__styles_gallery.check_styles_folder_exists()
+        
+        if   'message' in self.__client_data: 
+            self.__response_stylize_message(self.__initial_message),
+        if   'data' in self.__client_data: 
+            self.__apply_style(),
+        if   'delGalleryImg' in self.__client_data: 
+            self.__delete_stylesgallery_img(),
+        if   'styleFile' in self.__client_data:  
+            self.__add_stylegallery_img(),
+        if   'style_gallery' in self.__client_data: 
+            self.__reload_stylesgallery()
+        
+    def __make_instances(self):
+        self.__styles_gallery = StylesGallery(self.__obj_response)
+        self.__server_responder = ServerResponder(self.__obj_response)
+
+    def __response_stylize_message(self, text):
+        self.__server_responder.send_message(text)
+
+    def __apply_style(self):
+        self.__set_stylize_params()
+        self.__response_stylized_img()
+        self.__response_stylize_message(self.__cleared_massage)
+
+    def __set_stylize_params(self):
+        self.__CONTENT = self.__process_content_img()
+        self.__NETWORK = self.__get_network_path()
+
+    def __process_content_img(self):
+        content_processor = ContentImageProcessor(self.__client_data['data'][1])
+        content_processor.process_content_img()
+        return content_processor.get_content_img_path()
+   
+    def __get_network_path(self):
+        return os.path.join(app.root_path, "pretrained-networks/{}".format(self.__get_selected_style()))
+ 
+    def __get_selected_style(self):
+        return self.__client_data['data'][2].split('.')[0] 
+
+    def __response_stylized_img(self):
+        self.__server_responder.send_image(self.__get_stylized_img())
+                
+    def __get_stylized_img(self):
+        stylizer = ImageStylizer(self.__CONTENT, self.__NETWORK)
+        return stylizer.stylize_image()
+       
+    
+    
+    def __delete_stylegallery_img(self):
+        self.__styles_gallery.delete_style(self.__client_data['delGalleryImg'])
+
+
+    def __add_stylesgallery_img(self):
+        self.__styles_gallery.add_new_style(client_data['styleFile'][0], client_data['styleFile'][1])
+        self.__reload_stylesgallery()
+
+    
+    def __reload_stylesgallery(self):
+        self.__styles_gallery.reload_gallery_imgs()
+
+
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=63101, debug=True)
     http_server = WSGIServer(('', 63101), app)
--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 # Copyright (c) 2016-2017 Shafeen Tejani. Released under GPLv3.
+from os.path import exists
+from PIL import Image
+import PIL.Image
 import os
 import uuid
+import base64
 import numpy as np
 import scipy
 import scipy.misc
-from os.path import exists
+
 
 def load_image(image_path, img_size=None):
     assert exists(image_path), "image {} does not exist".format(image_path)
@@ -18,10 +22,6 @@ def load_image(image_path, img_size=None):
     img = img.astype("float32")
     return img
 
-
-def save_image(img, path):
-    scipy.misc.imsave(path, np.clip(img, 0, 255).astype(np.uint8))
-
 def get_files(img_dir):
     files = list_files(img_dir)
     return map(lambda x: os.path.join(img_dir,x), files)
@@ -34,16 +34,41 @@ def list_files(in_path):
 
     return files
 
-
-def rename_file(folder, prefix, file_ext):
-    filename = os.path.join(folder, prefix + str(uuid.uuid4())+ '.{}'.format(file_ext))
+def get_filename():
+    filename = os.path.join('stylized-', str(uuid.uuid4())+ '{}'.format('.jpg'))
     return filename
-
-def remove_file(filename, default_folder):
-    filepath = os.path.join(default_folder,filename)
-    if os.path.isfile(filepath):
-        os.remove(filepath)
 
 def write_file(f_name, file):
     with open(f_name, 'wb') as f:
         f.write(file)
+    
+def make_thumbnail(filename):
+    thumbnail_size = (300,300)
+    im = Image.open(filename)
+    im.thumbnail(thumbnail_size,  Image.ANTIALIAS)
+    im.save(filename)
+
+def decode_img(b64file, f_name):
+    if ',' in b64file:
+        imgdata = b64file.split(',')[1]
+        decoded = base64.b64decode(imgdata)
+        write_file(f_name, decoded)
+
+def encode_img(filename):
+    with open(filename, "rb") as image_file:
+        return to_base64(image_file.read())
+    
+def to_base64(img):
+    return base64.b64encode(img)
+
+def process_base64(base64_str):
+    base64_str = str(base64_str).split("b'")[-1].split("b'")[-1][:-1]
+    return add_charakters_to_base64_str(base64_str)
+
+def add_charakters_to_base64_str(base64_str, styles_gallery=True):
+    return 'data:image/png;base64,{}{}'.format(base64_str,'=') if base64_str.endswith('=') and styles_gallery else  'data:image/png;base64,{}'.format(base64_str)
+
+def png_to_3channels(filename):
+    image = PIL.Image.open(filename)
+    im = image.convert("RGB")
+    im.save(filename)
